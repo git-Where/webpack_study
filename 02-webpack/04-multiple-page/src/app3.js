9992cec8855ff7260eb2d7223c29cb8ffc67f4ee
yeqs <811373724@qq.com>
2022-07-30 19:33:55 +0800

    feat(p71/p72): 多页模板配置，多页环境搭建

--- 02-webpack/04-multiple-page/src/app3.js
+++ 02-webpack/04-multiple-page/src/app3.js
@@ -1,3 +1,2 @@
 import _ from 'lodash'
-console.log('1')
 console.log(_)
--- 02-webpack/04-multiple-page/webpack.config.js
+++ 02-webpack/04-multiple-page/webpack.config.js
@@ -1,27 +1,43 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const path = require('path')
 
 module.exports = {
   mode:'development',
-  entry:['./src/app.js','./src/app2.js','lodash'],
+  entry:{
+    main:{
+      // 以下加了dependOn的意思是：在app.js或者app2.js文件中要是有引入依赖（dependOn）lodash 则不在文件里进行打包，直接单独打包lodash
+      import:['./src/app.js','./src/app2.js'],
+      dependOn:'lodash',
+      filename:'channel1/[name].js'
+    },
+    main2:{
+      import:'./src/app3.js',
+      dependOn:'lodash', //lodash 指的是下面的key,
+      filename:'channel2/[name].js'
+    },
+    lodash:{
+      import:'lodash',
+      filename:'common/[name].js'
+    }
+  },
   plugins:[
-    new HtmlWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title:'多页面应用',
+      template:'./index.html',
+      filename:'channel1/index.html',
+      inject:'body', //打包之后在页面上的body内引入js文件
+      chunks:['main','lodash'], // 当前页面引入main.js文件
+      publicPath:'http://www.a.com/', // 文件在打包之后被script标签加载的前缀路劲
+    }),
+    new HtmlWebpackPlugin({
+      title:'多页面应用',
+      template:'./index2.html',
+      filename:'channel2/index2.html',
+      inject:'body', //打包之后在页面上的body内引入js文件
+      chunks:['main2','lodash'], // 当前页面引入main.js文件
+      publicPath:'http://www.b.com/'
+    })
   ],
-  // devtool:'inline-source-map',
-  // module:{
-  //   rules:[
-  //     {
-  //       test:/\.ts$/,
-  //       use:'ts-loader', // 解析.ts后缀的文件
-  //       exclude:/node_modules/, // 排除该文件夹下的.ts文件不做解析
-  //     }
-  //   ]
-  // },
-  // resolve:{
-  //   extensions:['.ts','.js'] //  优先解析ts文件，接着在解析js文件
-  // },
-  // output:{
-  //   filename:'bundle.js',
-  //   path:path.resolve(__dirname,'./dist')
-  // }
+  output:{
+    clean:true
+  }
 }
